63029ec617441e8c0f298590504d890ec2d71d72
ValentinoChandra <valentino.chandra@itbss.ac.id>
2023-05-13 22:12:34 +0700

    commit

--- register.php
+++ register.php
@@ -4,14 +4,13 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width,initial-scale=1.0">
-        <title> Masuk <title>
-        <link rel="stylesheet" href="stylea.css">
+        <title> Daftar <title>
     </head>
 
     <body>
         <div class="container">
             <form action="" method="POST" class="login-email">
-                <p style="font-size: 2rem; font-weight:850px"> Masuk </p>
+                <p style="font-size: 2rem; font-weight:850px"> DAFTAR </p>
                 <div class="input-group">
                     <input type="text" placeholder="User Name" name="username">
                 </div>
@@ -22,10 +21,16 @@
                     <input type="password" placeholder="Password" name="password">
                 </div>
                 <div class="input-group">
-                    <button name="submit" class="btn"> Masuk </button>
+                    <input type="password" placeholder="Password" name="password">
                 </div>
-                <p class="login-register-text"> Belum ada Akun?
-                <a href="register.php"> Daftar </a> </p>
+                <div class="input-group">
+                    <input type="password" placeholder="Comfirm Password" name="cppassword">
+                </div>
+                <div class="input-group">
+                    <button name="submit" class="btn"> Daftar </button>
+                </div>
+                <p class="login-register-text"> Sudah ada Akun?
+                <a href="index.php"> Masuk </a> </p>
             </form>
         </div>
     </body>
